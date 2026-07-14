8834b1c37afe97212bcab2ad657e0318537bda47
gouthm.04 <gouthm.04@gmail.com>
2026-07-14 18:39:48 +0530

    Initial backend setup

--- app.js
+++ app.js
@@ -1,2 +1,42 @@
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
 
+const app = express()
+app.use(cors())
+app.use(express.json())
 
+mongoose.connect("mongodb+srv://goutham:goutham123@cluster0.umdwywd.mongodb.net/hackathonTeamsDB")
+.then(
+    () => {
+        console.log("MongoDB Connected")
+    }
+).catch(
+    (error) => {
+        console.log(error)
+    }
+)
+
+const TeamData = mongoose.model("HackathonTeamDatas",new mongoose.Schema({
+    teamId : String,
+    Name : String,
+    leaderName : String,
+    leaderEmail : String,
+    leaderPhone : String,
+    college : String,
+    numOfMembers : String,
+    projectTitle : String,
+    problemStatement : String,
+    techStack : String,
+    mentorName : String,
+    regDate : String,
+    tableNo : String
+}))
+
+app.get("/test", (req,res) => {
+    res.send("Hello")
+})
+
+app.listen(3000, () => {
+    console.log("Server Started")
+})
